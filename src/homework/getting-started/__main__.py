f924c4cd619103fa33c37c86198419ffe342ccf2
CarlosBarbozaE <casoso10@hotmail.com>
2020-10-02 16:38:59 -0500

    HW: Getting started solution

---
+++ src/homework/getting-started/__main__.py
@@ -0,0 +1,8 @@
+
+import fire
+
+from .main import Hello
+
+
+if __name__ == "__main__":
+    fire.Fire(Hello)
---
+++ src/homework/getting-started/main.py
@@ -0,0 +1,8 @@
+MY_STUDENT_ID = "IF709907"
+
+
+class Hello:
+
+    @staticmethod
+    def hello():
+        return f"This is a greeting message from {MY_STUDENT_ID}."
